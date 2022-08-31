4369845d7961a293e1ba942860fc2eca284316db
Jordan Salazar <jcr.salazar@outlook.com>
2022-08-31 16:39:02 -0500

    feat: change footer message

--- util/app.js
+++ util/app.js
@@ -1,11 +1,12 @@
 const getYear = () => {
-    
-    const date = new Date();
-    const year = date.getFullYear();
+  const date = new Date();
+  const year = date.getFullYear();
 
-    return year;
-}
+  return year;
+};
 
 window.onload = () => {
-    document.getElementById("footer__title-h3").innerHTML = `Copyright &copy; ${getYear()} - RAFALAZAR`;
-}
+  document.getElementById(
+    'footer__title-h3'
+  ).innerHTML = `Made with &#128153; by RAFALAZAR\nCopyright &copy; ${getYear()}`;
+};
